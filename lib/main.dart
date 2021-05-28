6ddf383629ff553edccdd75cd8db61e2a4f20e7c
suixng <suixingjun@gmail.com>
2021-05-28 16:45:05 +0800

    widget of learn

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:flutter/rendering.dart';
 
 // class MyApp extends StatelessWidget {
 //   @override
@@ -421,23 +422,328 @@ class _ShoppingListState extends State<ShopppingList> {
 // }
 
 //Not Material App
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       decoration: BoxDecoration(color: Colors.white),
+//       child: Center(
+//         child: Text(
+//           'Hello World!',
+//           textDirection: TextDirection.ltr,
+//           style: TextStyle(
+//             fontSize: 32,
+//             color: Colors.black87,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+//Widget of the Row and the Column
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter layout demo',
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Flutter layout demo'),
+//         ),
+//         //Change to buildColumn() for the other column example
+//         //body: Center(child: buildRow()),
+//         //body: Center(child: buildColumn(),),
+//         //body: Center(child: buildOverflowingRow()),
+//         //body: Center(child: buildExpandedImages()),
+//         body: Center(child: buildExpandedImagesWithFlex()),
+//       ),
+//     );
+//   }
+
+//   Widget buildRow() =>
+//       //  #docregion Row
+//       Row(
+//         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//         children: [
+//           Image.asset('images/pic1.jpg'),
+//           Image.asset('images/pic2.jpg'),
+//           Image.asset('images/pic3.jpg'),
+//         ],
+//       );
+//   //  #enddocregion Row
+
+//   Widget buildColumn() =>
+//       //  #docregion Column
+//       Column(
+//         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//         children: [
+//           Image.asset('images/pic1.jpg'),
+//           Image.asset('images/pic2.jpg'),
+//           Image.asset('images/pic3.jpg'),
+//         ],
+//       );
+//   //  #enddocregion Column
+
+//   Widget buildOverflowingRow() =>
+//       // #docregion overflowing-row
+//       Row(
+//         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//         children: [
+//           Image.asset('images/pic1-1.jpg'),
+//           Image.asset('images/pic2-1.jpg'),
+//           Image.asset('images/pic3-1.jpg'),
+//         ],
+//       );
+//   // #enddocregion overflowing-row
+
+//   Widget buildExpandedImages() =>
+//       // #docregion expanded-images
+//       Row(
+//         crossAxisAlignment: CrossAxisAlignment.center,
+//         children: [
+//           Expanded(
+//             child: Image.asset('images/pic1-1.jpg'),
+//           ),
+//           Expanded(
+//             child: Image.asset('images/pic2-1.jpg'),
+//           ),
+//           Expanded(
+//             child: Image.asset('images/pic3-1.jpg'),
+//           ),
+//         ],
+//       );
+//   // #enddocregion expanded-images
+
+//   Widget buildExpandedImagesWithFlex() =>
+//       // #docregion expanded-images-with-flex
+//       Row(
+//         crossAxisAlignment: CrossAxisAlignment.center,
+//         children: [
+//           Expanded(
+//             child: Image.asset('images/pic1-1.jpg'),
+//           ),
+//           Expanded(
+//             flex: 2,
+//             child: Image.asset('images/pic2-1.jpg'),
+//           ),
+//           Expanded(
+//             child: Image.asset('images/pic3-1.jpg'),
+//           ),
+//         ],
+//       );
+//   // #enddocregion expanded-images-with-flex
+// }
+
+//Strwaberry Pavlova Recipe
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter layout demo',
+//       home: buildHomePage('Strawberry Pavlova Recipe'),
+//     );
+//   }
+
+//   Widget buildHomePage(String title) {
+//     final titleText = Container(
+//       padding: EdgeInsets.all(20),
+//       child: Text(
+//         'Strawberry Pavlova',
+//         style: TextStyle(
+//           fontWeight: FontWeight.w800,
+//           letterSpacing: 0.5,
+//           fontSize: 30,
+//         ),
+//       ),
+//     );
+
+//     final subTitle = Text(
+//       'Pavlova is a meringue-based dessert named after the Russian ballerina '
+//       'Anna Pavlova. Pavlova features a crisp crust and soft, light inside, '
+//       'topped with fruit and whipped cream.',
+//       textAlign: TextAlign.center,
+//       style: TextStyle(
+//         fontFamily: 'Georgia',
+//         fontSize: 25,
+//       ),
+//     );
+
+//     // #docregion ratings, stars
+//     var stars = Row(
+//       mainAxisSize: MainAxisSize.min,
+//       children: [
+//         Icon(Icons.star, color: Colors.green[500]),
+//         Icon(Icons.star, color: Colors.green[500]),
+//         Icon(Icons.star, color: Colors.green[500]),
+//         Icon(Icons.star, color: Colors.black),
+//         Icon(Icons.star, color: Colors.black),
+//       ],
+//     );
+//     // #enddocregion stars
+
+//     final ratings = Container(
+//       padding: EdgeInsets.all(20),
+//       child: Row(
+//         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//         children: [
+//           stars,
+//           Text(
+//             '170 Reviews',
+//             style: TextStyle(
+//               color: Colors.black,
+//               fontWeight: FontWeight.w800,
+//               fontFamily: 'Roboto',
+//               letterSpacing: 0.5,
+//               fontSize: 20,
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//     // #enddocregion ratings
+
+//     // #docregion iconList
+//     final descTextStyle = TextStyle(
+//       color: Colors.black,
+//       fontWeight: FontWeight.w800,
+//       fontFamily: 'Roboto',
+//       letterSpacing: 0.5,
+//       fontSize: 18,
+//       height: 2,
+//     );
+
+//     // DefaultTextStyle.merge() allows you to create a default text
+//     // style that is inherited by its child and all subsequent children.
+//     final iconList = DefaultTextStyle.merge(
+//       style: descTextStyle,
+//       child: Container(
+//         padding: EdgeInsets.all(20),
+//         child: Row(
+//           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//           children: [
+//             Column(
+//               children: [
+//                 Icon(Icons.kitchen, color: Colors.green[500]),
+//                 Text('PREP:'),
+//                 Text('25 min'),
+//               ],
+//             ),
+//             Column(
+//               children: [
+//                 Icon(Icons.timer, color: Colors.green[500]),
+//                 Text('COOK:'),
+//                 Text('1 hr'),
+//               ],
+//             ),
+//             Column(
+//               children: [
+//                 Icon(Icons.restaurant, color: Colors.green[500]),
+//                 Text('FEEDS:'),
+//                 Text('4-6'),
+//               ],
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//     // #enddocregion iconList
+
+//     // #docregion leftColumn
+//     final leftColumn = Container(
+//       padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
+//       child: Column(
+//         children: [
+//           titleText,
+//           subTitle,
+//           ratings,
+//           iconList,
+//         ],
+//       ),
+//     );
+//     // #enddocregion leftColumn
+
+//     final mainImage = Image.asset(
+//       'images/pavlova.jpg',
+//       fit: BoxFit.cover,
+//     );
+
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(title),
+//       ),
+//       // #docregion body
+//       body: Center(
+//         child: Container(
+//           margin: EdgeInsets.fromLTRB(0, 40, 0, 30),
+//           height: 600,
+//           child: Card(
+//             child: Row(
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               children: [
+//                 Container(
+//                   width: 440,
+//                   child: leftColumn,
+//                 ),
+//                 mainImage,
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//       // #enddocregion body
+//     );
+//   }
+// }
+
+//Container
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(color: Colors.white),
-      child: Center(
-        child: Text(
-          'Hello World!',
-          textDirection: TextDirection.ltr, 
-          style: TextStyle(
-            fontSize: 32,
-            color: Colors.black87,
-          ),
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter layout demo'),
         ),
+        body: Center(child: _buildImageColumn()),
       ),
     );
   }
+
+  // #docregion column
+  Widget _buildImageColumn() => Container(
+        decoration: BoxDecoration(
+          color: Colors.black26,
+        ),
+        child: Column(
+          children: [
+            _buildImageRow(4),
+            _buildImageRow(6),
+          ],
+        ),
+      );
+  // #enddocregion column
+
+  // #docregion row
+  Widget _buildDecoratedImage(int imageIndex) => Expanded(
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(width: 10, color: Colors.black38),
+            borderRadius: const BorderRadius.all(const Radius.circular(8)),
+          ),
+          margin: const EdgeInsets.all(4),
+          child: Image.asset('images/pic$imageIndex.jpg'),
+        ),
+      );
+
+  Widget _buildImageRow(int imageIndex) => Row(
+        children: [
+          _buildDecoratedImage(imageIndex),
+          _buildDecoratedImage(imageIndex + 1),
+        ],
+      );
+  // #enddocregion row
 }
 
 void main() {
@@ -510,5 +816,7 @@ void main() {
   //   ),
   // );
 
+  //debugPaintSizeEnabled = true; //Remove to supress visual layout
+  debugPaintSizeEnabled = false; // Set to true for visual layout
   runApp(MyApp());
 }
