f3ebe254e2ee0f1bd7833010ca3dbfc45880d85a
suixng <suixingjun@gmail.com>
2021-06-04 16:38:15 +0800

    A demo APp

--- lib/main.dart
+++ lib/main.dart
@@ -949,6 +949,21 @@ class MyApp extends StatelessWidget {
       ),
     );
 
+    // #docregion textSection
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+    // #enddocregion textSection
+
     return MaterialApp(
       title: "Flutter layout demo",
       home: Scaffold(
@@ -958,10 +973,17 @@ class MyApp extends StatelessWidget {
         // body: Center(
         //   child: Text('Hello World'),
         // ),
-        body: Column(
+        body: ListView(
           children: [
+            Image.asset(
+              'images/lake.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
             titleSection,
             buttonSection,
+            textSection,
           ],
         ),
       ),
